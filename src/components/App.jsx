a9ea2d1c127c3d96af1ba1db7c98c5850d76ecda
AnnaNesterenko <nura_arh@ukr.net>
2022-07-30 17:26:08 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
-class App extends Component {
+export class App extends Component {
   state = {
-    image: [],
+    pictures: [],
   };
-
-  //   componentDidMount() {
-  //     fetch(
-  //       'https://pixabay.com/api/?key=28157961-50aacf6d1ff0efe2e77cab6d2&q=yellow+flowers&image_type=photo'
-  //     )
-  //       .then(res => res.json())
-  //       .then(image => this.setState(image));
-  //   }
 
   render() {
     return <div>App</div>;
   }
 }
-
-export default App;
