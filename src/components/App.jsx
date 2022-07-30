c6d0cb4805cf59d2d77655bb89a351e8389cc5b3
AnnaNesterenko <nura_arh@ukr.net>
2022-07-30 13:58:30 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    image: [],
+  };
+
+  //   componentDidMount() {
+  //     fetch(
+  //       'https://pixabay.com/api/?key=28157961-50aacf6d1ff0efe2e77cab6d2&q=yellow+flowers&image_type=photo'
+  //     )
+  //       .then(res => res.json())
+  //       .then(image => this.setState(image));
+  //   }
+
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
