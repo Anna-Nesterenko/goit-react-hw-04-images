3492e0b0aaf4fa320cacc5bb91262a229d7f0b1f
AnnaNesterenko <nura_arh@ukr.net>
2022-08-11 13:30:54 +0300

    changeModalHook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,15 +6,15 @@ import PropTypes from 'prop-types';
 const modalRoot = document.querySelector('#modal-root');
 function Modal({ children, onCloseModal }) {
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onCloseModal();
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
-  });
-
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onCloseModal();
-    }
-  };
+  }, [onCloseModal]);
 
   const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
